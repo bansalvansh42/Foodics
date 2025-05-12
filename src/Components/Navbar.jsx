1f51f9fff7e7fedbc51cf520e56ea5e277dde241
bansalvansh42 <89079920+bansalvansh42@users.noreply.github.com>
2025-05-12 18:38:56 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <nav className="flex flex-col lg:flex-row items-center justify-between px-6 py-4 bg-white shadow-md rounded-lg border border-violet-100">
       <div className="mb-3 lg:mb-0">
         <h3 className="text-sm text-gray-500">{new Date().toUTCString().slice(0, 16)}</h3>
-        <h1 className="text-2xl font-extrabold text-indigo-600">Foodics</h1>
+        <h1 className="text-2xl font-extrabold text-indigo-600">CraveBane</h1>
       </div>
       <div className="w-full lg:w-[30vw]">
         <input
