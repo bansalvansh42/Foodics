ad1cfcda9847405555aba8a6e1b84256c346cf82
bansalvansh42 <bansalvansh2003@gmail.com>
2025-05-12 18:08:06 +0530

    first commit

--- src/Components/CategoryMenu.jsx
+++ src/Components/CategoryMenu.jsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from "react";
-import FoodData from "../data/FoodData";
+import FoodData from "../Data/FoodData";
 import { useDispatch, useSelector } from "react-redux";
 import { setCategory } from "../redux/Slices/CategorySlice";
 
